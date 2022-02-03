36e7ca5927dfae84dc2438a2a9d59ea17be5fad5
lucas-malla <lucasarien.9210@gmail.com>
2022-02-03 11:27:22 -0300

    mercado-liebre

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log('Servidor CORRIENDO EN EL PUERTO 3000');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
